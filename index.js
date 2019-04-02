89ddca2fd533897c1db6d8de48f3c75d0ee9f905
Srija S <srijakumar7@gmail.com>
2019-04-02 22:57:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,10 @@ function findMinAndRemove(array){
 }
 
 function insertionSort(array){
-
+  let newMin;
+  let sorted = []
+  while(array.length != 0){
+    newMin = minAndRemove(array)sorted.push(newMin)
+  }
+  return sorted
 }
